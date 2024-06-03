6720d11f3536a0decfc138612994779f9dcee10d
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2024-06-03 16:28:00 +0200

    redo processing for v2024-1 of the API

--- src/process_manifests.py
+++ src/process_manifests.py
@@ -12,20 +12,43 @@ Dataset schema
 }
 """
 
+# %%
 from tqdm import tqdm
 import pandas as pd
 import datasets
 
 
-# load csv
-df = pd.read_csv("data/raw/mp_corpus_2000-01-01.csv")
-# remove index
-df = df.drop(columns=["Unnamed: 0"])
+# %%
+df = pd.read_csv("data/raw/mp_corpus_1990-01-01.csv")
+meta = pd.read_csv("data/raw/meta_corpus_1990-01-01.csv")
+parties = pd.read_csv("data/raw/mp_parties.csv")
 
+# %%
+# select countries of interests from the party metadata table
+countries_of_interest = [
+    "Italy", "Serbia", "Georgia", "Croatia", "Ukraine", "Poland", "Spain", "Montenegro",
+    "Latvia", "Netherlands", "Romania", "Slovakia", "North Macedonia", "Lithuania", "Bulgaria",
+    "Belgium", "Czech Republic", "Slovenia", "Russia", "France", "Armenia", 
+    "Turkey", "Bosnia-Herzegovina", "Estonia", "Greece", "Portugal", "Ireland", "Denmark",
+    "Germany", "Iceland", "Switzerland", "Moldova", "German Democratic Republic", "Finland",
+    "United Kingdom", "Hungary", "Albania", "Norway", "Cyprus", "Austria", "Sweden",
+    "Luxembourg", "Belarus", "Azerbaijan", "Northern Ireland", "Malta",
+    ]
 
-# v1: merge text chunks together into a single manifesto
+parties_subset = parties.query("countryname == @countries_of_interest")
+parties_of_interest = set(parties_subset["party"].tolist())
+
+# select parties to process from df
+df_subset = df[df["party"].isin(parties_of_interest)]
+
+# %%
+# select valid manifesto_ids from the manifesto metadata table
+meta_subset = meta.dropna(subset=["manifesto_id"])
+assert len(set(meta_subset["manifesto_id"])) == len(meta_subset)
+
+# %%
 v1 = []
-for manifesto_id, gr_df in tqdm(df.groupby("manifesto_id")):
+for manifesto_id, gr_df in tqdm(df_subset.groupby("manifesto_id")):
     # concat text
     concat_text = " ".join(gr_df["text"].tolist())
     # turn annotations into a list
@@ -34,28 +57,33 @@ for manifesto_id, gr_df in tqdm(df.groupby("manifesto_id")):
     n_words = len(concat_text.split(" "))
     # representative observation
     obs = gr_df.reset_index().head(1)
+    # find additional manifesto metadata
+    obs_manifesto_meta = meta_subset[meta_subset["manifesto_id"] == manifesto_id].reset_index()
+    # find additional party metadata
+    obs_party_meta = parties_subset[parties_subset["party"] == obs["party"][0]].reset_index()
 
     new_obs = {
         "id": manifesto_id,
         "text": concat_text,
-        "source": "mp_corpus_2000-01-01",
-        "added": "2024-02-28 00:00:00",
+        "source": obs_manifesto_meta["source"][0],
+        "added": "2024-06-03 00:00:00",
         "created": str(pd.to_datetime(obs["date"][0], format="%Y%m")),
         "metadata": {
-            "cmp_codes": cmp_codes,
-            "party": obs["party"][0],
+            "country_code": obs_party_meta["country"][0],
+            "country_name": obs_party_meta["countryname"][0],
+            "party_code": obs["party"][0],
+            "party_abbrev": obs_party_meta["abbrev"][0],
+            "party_name": obs_party_meta["name"][0],
             "language": obs["language"][0],
-            "title": obs["title"][0],
+            "title": obs_manifesto_meta["title"][0],
             "n_words": n_words,
+            "cmp_codes": cmp_codes,
+            "translation_en": obs["translation_en"][0],
         },
     }
 
     v1.append(new_obs)
 
-
-ds_v1 = datasets.Dataset.from_list(v1)
-ds_v1.save_to_disk("data/interim/mp_2000")
-
-
-
-# v2: new version of the API
+df_v1 = pd.DataFrame.from_records(v1)
+ds_v1 = datasets.Dataset.from_pandas(df_v1)
+ds_v1.save_to_disk("data/interim/mp_1990")
